de41f22487c47177e4a735e66c41fd9877d91741
VinteKls_Dev <81206911+20Kg-3s@users.noreply.github.com>
2021-05-26 16:24:25 -0300

    finalizada atividade 5

--- src/com/company/atividade5.java
+++ src/com/company/atividade5.java
@@ -8,19 +8,50 @@ public class atividade5 {
         // Armazene os números pares no vetor PAR e os números IMPARES no vetor impar. Imprima os três vetores.
         int parPosicao = -1;
         int impPosicao = -1;
-        int[] numGeral = new int[10];
+        int[] todosNumeros = new int[10];
         int[] numPar = new int[10];
         int[] numImpar = new int[10];
+        boolean verificarNumerosB = false;
 
-
-
-        System.out.println("digite 10 valores:");
+        System.out.println("digite 10 valores para serem divididos em par e ímpar:");
         Scanner sc = new Scanner(System.in);
-        for (int i=0;i<numGeral.length;i++){
-            numGeral[i] = sc.nextInt();
+        for (int i = 0; i< todosNumeros.length; i++){
+            todosNumeros[i] = sc.nextInt();
         }
-        for (int i=0;i<numGeral.length;i++){
+        for (int i = 0; i< todosNumeros.length; i++){
+            int verificarNumeros = todosNumeros[i];
+            verificarNumerosB = true;
 
+            while(verificarNumerosB){
+                if(verificarNumeros == 0){
+                    parPosicao++;
+                    numPar[parPosicao] = todosNumeros[i];
+                    verificarNumerosB = false;
+                    break;
+
+                }else if (verificarNumeros == 1){
+                    impPosicao++;
+                    numImpar[impPosicao] = todosNumeros[i];
+                    verificarNumerosB = false;
+                    break;
+                }else {
+                    verificarNumeros = verificarNumeros -2;
+                }
+            }
+        }
+        System.out.println("Numeros informados ao sistema: ");
+        for (int i = 0; i< todosNumeros.length; i++){
+            System.out.print(todosNumeros[i] + " ");
+        }
+
+        System.out.println("\nValores Pares recebidos: ");
+        for (int i = 0; i< todosNumeros.length; i++){
+            System.out.print(numPar[i] + " ");
+        }
+
+        System.out.println("\nValores ímpar recebidos");
+        for (int i = 0; i< todosNumeros.length; i++){
+            System.out.print(numImpar[i] + " ");
         }
     }
 }
